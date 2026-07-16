419d8814d3974364cd59ff3592f8b49ebcbfefdb
Felipe Oliveira <felipe.oliveira@lioraenergia.com.br>
2026-07-16 07:18:09 -0300

    Stefanny: aprovada em 15/07 (ontem)

--- build/process_mobile.py
+++ build/process_mobile.py
@@ -102,7 +102,7 @@ CONSUMPTION_OVERRIDE = {  # cliente (upper/strip) -> MWh; temp ate base corrigir
  'MÁRCIO PEREIRA PINTO': 5.866,  # aprovado manual, base mostra 1.3 (Felipe 10/07)
 }
 FORCE_APPROVED = {  # deal_id -> nota; contam como aprovado por decisao manual (pre-BGC)
- 'b61609e9-835a-4ed2-896d-2de4fd55c4f9': '2026-07-16',  # Stefanny Karoline Martins - aprovado manual, Mirla/RN Interior (Felipe 16/07)
+ 'b61609e9-835a-4ed2-896d-2de4fd55c4f9': '2026-07-15',  # Stefanny Karoline Martins - aprovado manual, Mirla/RN Interior (Felipe 16/07)
  '9ccf72c4-32d9-406f-b2c1-a533f486c8d1': '2026-07-14',  # Antonio Edmilson Leite - dup denied em BGC (Felipe 15/07)
  'a4a2cc08-a77e-4549-8306-ef13db2a3a95': '2026-07-10',  # Marcio Pereira pinto / Nicola Popovic (Felipe 10/07)
 }
@@ -114,7 +114,7 @@ LOST_IGNORE = {  # ignora lost_at/lost_reason (falso 'nao aceito pela distribuid
 INJECT_DEALS = [
  {
  'deal_id':'b61609e9-835a-4ed2-896d-2de4fd55c4f9','deal_stage':'BACKGROUND_CHECKING','deal_lost_at':'','deal_lost_reason':'',
- 'rd_station_crm_id':'','deal_created_at':'2026-07-16T00:00:00','current_client_cnpj':'','current_client_cpf':'',
+ 'rd_station_crm_id':'','deal_created_at':'2026-07-15T00:00:00','current_client_cnpj':'','current_client_cpf':'',
  'current_client_name':'STEFANNY KAROLINE MARTINS DE S. MARTINS','client_phone_number':'',
  'current_client_state':'RN','current_client_city':'MOSSORÓ','distributor_short_name':'NEOENERGIA COSERN',
  'origin_campaign':'','origin_source':'','internal_sales_classification':'Direto','sales_team':'Field Sales',
@@ -125,7 +125,7 @@ INJECT_DEALS = [
  'consumption_group':'2. <= 1.0 MWh','proposal_id':'','proposal_created_at':'','accepted_proposal':'true',
  'product_name':'','energy_retailer_name':'Liora Energia','has_valid_bill_uploaded':'false','bill_id':'',
  'latest_contract_id':'','latest_contract_created_at':'','latest_contract_signature_signed_at':'',
- 'latest_risk_analysis_result':'','latest_risk_analysis_created_at':'2026-07-16T00:00:00',
+ 'latest_risk_analysis_result':'','latest_risk_analysis_created_at':'2026-07-15T00:00:00',
  'latest_risk_analysis_comments':'','idle_days':'0','idle_days_group':'','cancelation_date':'',
  'ops_tt_status':'','ops_tt_status_reason':'','credit_product':'0',
 },
