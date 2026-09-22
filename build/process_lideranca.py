189382e91c81e95f72e606dcf0e0f3627991d3f7
Felipe Oliveira <felipe.oliveira@lioraenergia.com.br>
2026-09-22 16:06:04 -0300

    Lote 22/09 -> 21/09: + ERIKA GOMES PEREIRA (Anderson Correia)
    Anderson Correia 4.49 -> 8.41 MWh camp. na 15-21/09: CRUZA a faixa de 8,
    R$ 0 -> R$ 1.500. Total da campanha: R$ 18.250 -> R$ 19.750.
    FORCE_APPROVED + LEDGER_IGNORE no mesmo commit. Remover na virada de mes.

--- build/process_lideranca.py
+++ build/process_lideranca.py
@@ -459,6 +459,7 @@ LEDGER_IGNORE = {
     '26f124d7-6662-4acd-9d19-79696e814d3d',  # ASSOC. BRASILEIRA DE PLANEJ. EM EFICIENCIA ENERGETICA LUZ DI (Conceicao Santos, 13.84 MWh)
     '057471e4-dfe2-4c4b-87db-a525e57b6404',  # ASSOC. BRASILEIRA DE PLANEJ. EM EFICIENCIA ENERGETICA (Conceicao Santos, 1.755 MWh)
     '4ba9faf6-827f-4b17-98c4-8bdde13e9bba',  # DEIA MINIMERCADO LTDA (Paulo Alexandre Jorge, 1.883 MWh)
+    'ecae8e1b-25cf-4268-b61d-da32950bb1f7',  # ERIKA GOMES PEREIRA (Anderson Correia, 3.915 MWh)
 }
 
 def _load_prev_aprov():
